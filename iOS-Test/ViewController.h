e69e7ba3bc974e693137ba5473bdb183096e8309
nagamobiledeveloper <naga.mobiledeveloper@gmail.com>
2013-08-01 10:35:36 -0500

    Initial Commit

---
+++ iOS-Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iOS-Test
+//
+//  Created by Seshu on 8/1/13.
+//  Copyright (c) 2013 Nagaseshu Vadlapudi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iOS-Test/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iOS-Test
+//
+//  Created by Seshu on 8/1/13.
+//  Copyright (c) 2013 Nagaseshu Vadlapudi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
